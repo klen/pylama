7a767cb34152f7782b5cb62d87ae723f741bce0f
Kirill Klenov <horneds@gmail.com>
2014-03-24 18:51:01 +0700

    Fix import in pyflakes

--- pylama/lint/pylama_pyflakes/__init__.py
+++ pylama/lint/pylama_pyflakes/__init__.py
@@ -9,14 +9,14 @@ from .. import Linter as BaseLinter
 path = op.dirname(op.abspath(__file__))
 sys.path.insert(0, path)
 
+from pyflakes import messages, checker
+
 
 class Linter(BaseLinter):
 
     """ Pyflakes code check. """
 
     def __init__(self):
-        from pyflakes import messages
-
         if messages.UndefinedName.message != "E0602 undefined name %r":
             monkey_patch_messages(messages)
 
@@ -29,7 +29,6 @@ class Linter(BaseLinter):
         """
         import _ast
         import os
-        from pyflakes import checker
 
         os.environ.setdefault('PYFLAKES_BUILTINS', builtins)
 
