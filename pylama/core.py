97a70a6f41825ac1761c32d8da4c2e7a6a70aec7
Kirill Klenov <horneds@gmail.com>
2015-06-30 19:51:41 +0300

    Fix

--- pylama/core.py
+++ pylama/core.py
@@ -111,8 +111,8 @@ def prepare_params(modeline, fileconfig, options):
     """
     params = dict(skip=False, ignore=[], select=[], linters=[])
     if options:
-        params['ignore'] = options.ignore[:]
-        params['select'] = options.select[:]
+        params['ignore'] = list(options.ignore)
+        params['select'] = list(options.select)
 
     for config in filter(None, [modeline, fileconfig]):
         for key in ('ignore', 'select', 'linters'):
